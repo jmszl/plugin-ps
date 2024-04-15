e392b64f2d61deb40f624c0c6ec36e14bbdd82af
tbz <tianbaozhen@foxmail.com>
2024-04-15 14:09:17 +0800

    fix: psStream为nil时报错

--- main.go
+++ main.go
@@ -89,6 +89,10 @@ func (c *PSConfig) ServeTCP(conn net.Conn) {
 				puber.PushPS(data)
 				return
 			} else {
+				if psStream == nil {
+					fmt.Println("psStream nil")
+					return
+				}
 				PSPlugin.Warn("publisher not found", zap.Uint32("ssrc", psStream.SSRC))
 				cache = append(cache, append([]byte(nil), data...))
 				if time.Since(startTime) > time.Second*5 {
